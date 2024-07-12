c684140c4ecd6609bda9f4b5112706618e5550f8
priyanshu30122001 <priyanshupadeliya27@gmail.com>
2024-07-13 03:01:43 +0530

    completed backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require("cors")
 dotenv.config();
 const port = process.env.PORT;
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://localhost:5173/",
     Credentials:true
 }));
 app.use(bodyParser.json());
