7ed994401c41c79ce74406eb525b009b98dcd990
priyanshu30122001 <priyanshupadeliya27@gmail.com>
2024-07-15 18:47:51 +0530

    made some changes

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -11,16 +11,15 @@ router.post("/login", async( req, res,next)=>{
     // throw new Error("this is error ");
     const user = await User.findOne({email});
     if(!user){
-        return res.status(400).json({message:"User Not Found  "});
+      res.status(400).json({message:"User Not Found  "});
     } 
     const validPassword = await bcrypt.compare(password,user.password);
-    if(!validPassword){
-        return res.status(400).json({message:"wrong password"});
+    if (!validPassword){
+      res.status(400).json({message:"wrong password"});
     }
     else{
         const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
         res.json({message:"Logged in successfully",token:token});
-        // console.log(token);
     }
   }
   catch(err){
